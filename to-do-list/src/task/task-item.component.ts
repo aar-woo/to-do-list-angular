253f7344e702f4fdb3d860a31bbbbefec72b09b3
aar-woo <awooyes@gmail.com>
2025-04-23 22:30:32 -0700

    Render task item components based on signal state and add task items with add button.

--- to-do-list/src/task/task-item.component.ts
+++ to-do-list/src/task/task-item.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
+interface Task {
+  id: number;
+  title: string;
+}
 @Component({
   selector: 'task-item',
   templateUrl: './task-item.component.html',
   styleUrl: './task-item.component.css',
 })
-export class TaskItemComponent {}
+export class TaskItemComponent {
+  @Input() task!: Task;
+}
