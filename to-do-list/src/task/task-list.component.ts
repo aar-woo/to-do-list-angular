9183a2656b2050058f516fd21490f16bd31923e3
aar-woo <awooyes@gmail.com>
2025-04-23 21:52:46 -0700

    Add basic task item component, text input item.

--- to-do-list/src/task/task-list.component.ts
+++ to-do-list/src/task/task-list.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TaskItemComponent } from './task-item.component';
 
 @Component({
   selector: 'task-list',
   templateUrl: './task-list.component.html',
+  imports: [TaskItemComponent],
   styleUrl: './task-list.component.css',
 })
 export class TaskListComponent {}
