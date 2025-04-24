a4ff210024aa5ddcbd8e7ecea18fe1f12cd33caf
aar-woo <awooyes@gmail.com>
2025-04-23 21:28:50 -0700

    Created initial component, task list.

---
+++ to-do-list/src/task/task-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'task-list',
+  templateUrl: './task-list.component.html',
+  styleUrl: './task-list.component.css',
+})
+export class TaskListComponent {}
